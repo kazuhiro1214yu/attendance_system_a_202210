f706d094f18aed4e5a3c5c1b1a337ccb496ed06d
kazuhiro1214yu <kazuhiro1214yu@gmail.com>
2022-04-27 12:30:49 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,11 @@
 module ApplicationHelper
   
+# 各viewを表示したときの｢タイトル｣をつけるためのメソッド｡
+# full_titleメソッドを､application.htmlファイルの<titleタグ>の中に入れることで､
+# 各viewファイルに記述した｢provide｣メソッドが反映して各viewファイルがシンプルな記述で収まる｡
   def full_title(page_name = "")
     base_title = "AttendanceApp"
-    if page_name.empty?
+    if page_name.empty? # 引数を受け取っているか判定(引数　=　page_name)
       base_title
     else
       page_name + "|" + base_title
