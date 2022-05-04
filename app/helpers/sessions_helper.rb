2f0295343877a26f87aeb5fbbc0c448ca88ee535
kazuhiro1214yu <kazuhiro1214yu@gmail.com>
2022-05-04 06:23:27 +0000

    6.3.2-finish

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,15 @@
 module SessionsHelper
-  
+
   # 引数に渡されたユーザーオブジェクトでログインします。
   def log_in(user)
     session[:user_id] = user.id
-  end 
-  
+  end
+
+  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+  # @current_userがviewやcontrollerで活用できるようにするためのメソッド
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
