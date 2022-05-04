8acc067e566848cd5452c73032ef602eed4873b7
kazuhiro1214yu <kazuhiro1214yu@gmail.com>
2022-05-04 06:14:31 +0000

    6.3.1-finish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ログイン後にユーザー情報ページにリダイレクトします。
+      # sessions_helperの　log_inメソッドで､
+      # session(※ブラウザのcokies)にログイン情報(user　id)を保存することが可能
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "認証に失敗しました。"
       render :new
