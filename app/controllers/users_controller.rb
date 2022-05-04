19b26223fc22ebc80b53d04c1da499da6a515b36
kazuhiro1214yu <kazuhiro1214yu@gmail.com>
2022-05-04 06:50:02 +0000

    Finish login_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user #新規登録して保存成功後､ログインも同時に行います(sessionにuser.idを保存すること)
       flash[:success] = "新規作成に成功しました｡"
       redirect_to @user
       # 保存に成功した場合は、ここに記述した処理が実行されます。
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,11 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  def log_out
+    session.delete(:user_id) #ブラウザのcokiesに保存しているsessionのuser.idだけ削除してもだめ
+    @current_user = nil #使い回しやすいようにcurrent_userメソッドで保存した@current_userも削除が必要
+  end 
 
   # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
   # @current_userがviewやcontrollerで活用できるようにするためのメソッド
