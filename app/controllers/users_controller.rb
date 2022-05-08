7ae1d2d4afd427128c746b7774b4a064e7caaa97
kazuhiro1214yu <kazuhiro1214yu@gmail.com>
2022-05-08 03:19:28 +0000

    10.2-finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,10 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
-  def show
-  end
+def show
+  @first_day = Date.current.beginning_of_month
+  @last_day = @first_day.end_of_month
+end
 
   def new
     @user = User.new
