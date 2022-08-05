ee2e234f189f21ad3336fd4af5493f4b07b1b620
kazuhiro1214yu <kazuhiro1214yu@gmail.com>
2022-08-05 14:59:10 +0000

    zangyo-shiji_shijisya_ui_finish

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -39,7 +39,7 @@ class AttendancesController < ApplicationController
     flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
     redirect_to user_url(date: params[:date])
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
-    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    flash[:danger] = "出社時間と退社時間に無効な入力データあり｡更新エラー。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
 
