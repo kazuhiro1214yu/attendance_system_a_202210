aeca6720ee99dc1df948b0ee54b5060c5e7d72bf
kazuhiro1214yu <kazuhiro1214yu@gmail.com>
2022-08-24 08:24:18 +0000

    tsuika_kinou-7-finish

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -32,14 +32,14 @@ class AttendancesController < ApplicationController
   def update_one_month
     ActiveRecord::Base.transaction do # トランザクションを開始します。
       attendances_params.each do |id, item|
-        attendance = Attendance.find(id)
-        attendance.update_attributes!(item)
+        @attendance = Attendance.find(id)
+        @attendance.update_attributes!(item)
       end
     end
     flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
     redirect_to user_url(date: params[:date])
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
-    flash[:danger] = "出社時間と退社時間に無効な入力データあり｡更新エラー。"
+    flash[:danger] = "出社時間と退社時間に無効な入力データあり｡更新エラー。<br>" + @attendance.errors.full_messages.join("<br>")
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
 
