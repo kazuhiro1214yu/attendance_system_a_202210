11af53b8b500aefd5184ecb8d9efdb338c10680f
kazuhiro1214yu <kazuhiro1214yu@gmail.com>
2022-09-12 14:27:42 +0000

    attendance.rb-validate-add

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,18 +5,26 @@ class Attendance < ApplicationRecord
   validates :note, length: { maximum: 50 }
 
 
-  # 出勤時間が存在しない場合、退勤時間は無効
+  # ①出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
-  # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
+  # ②出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
+  # ③勤怠編集で退勤時間が存在しない場合､出勤時間は無効　22/9/12追加　#
+  validate :started_at_is_invalid_without_a_finished_at
 
+  # ①#
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
   end
-
+  # ②#
   def started_at_than_finished_at_fast_if_invalid
     if started_at.present? && finished_at.present?
       errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
     end
   end
+  # ③#
+  def started_at_is_invalid_without_a_finished_at
+    errors.add(:finished_at, "が必要です") if started_at.present? && finished_at.blank?
+  end
+  
 end
